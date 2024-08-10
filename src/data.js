7d25b6e750dbd415e2c8a6308b6742721718467a
Your GitHub Username <bansalram7847@gmail.com>
2024-08-10 11:57:15 +0530

    first commit

---
+++ src/data.js
@@ -0,0 +1,67 @@
+const data = [
+	{
+		id: 1,
+		name: "Agra",
+		info: "Agra is a city in northern India, known for the iconic Taj Mahal, a stunning white marble mausoleum built by Mughal emperor Shah Jahan in memory of his beloved wife. Agra is also home to several other historic and architectural wonders, including Agra Fort and Fatehpur Sikri.",
+		image: "https://cdn.thecodehelp.in/Agra.jpeg",
+		price: "35,758",
+	},
+	{
+		id: 2,
+		name: "Jaipur",
+		info: `Jaipur is the capital city of the Indian state of Rajasthan, known for its stunning pink-hued buildings and palaces. The city is also known as the "Pink City" due to the color of many of its buildings. Jaipur is famous for its intricate textiles, colorful handicrafts, and delicious cuisine.`,
+		image: "https://cdn.thecodehelp.in/Jaipur.jpeg",
+		price: "82,560",
+	},
+	{
+		id: 3,
+		name: "Goa",
+		info: " Goa is a small state on India's western coast, known for its stunning beaches, vibrant nightlife, and Portuguese-influenced architecture. The state is also known for its spicy seafood dishes and unique cultural traditions.",
+		image: "https://cdn.thecodehelp.in/Goa.jpeg",
+		price: "29,695",
+	},
+	{
+		id: 4,
+		name: "Varanasi",
+		info: "Varanasi is a holy city in northern India, situated on the banks of the sacred river Ganges. The city is a major pilgrimage site for Hindus and is famous for its ancient temples, winding alleyways, and colorful festivals.",
+		image: "https://cdn.thecodehelp.in/Varanasi.jpeg",
+		price: "31,095",
+	},
+	{
+		id: 5,
+		name: "Darjeeling",
+		info: "Starting in the colorful port city of Gdańsk, you'll escape the crowds and embrace the understated elegance of ready-for-prime-time Poland for 10 days. With an expert Rick Steves guide at your side, you'll experience mighty Malbork castle, the cobbly-cute village of Toruń, Poland's contemporary capital of Warsaw, the spiritual Jasna Góra Monastery, and charming Kraków — Poland's finest city. In this land of surprises — so trendy and hip, yet steeped in history — there's so much to discover. Join us for the Best of Poland in 10 Days!",
+		image: "https://cdn.thecodehelp.in/Darjeeling.jpeg",
+		price: "78,595",
+	},
+	{
+		id: 6,
+		name: "Jaisalmer",
+		info: "Jaisalmer is a city in the Indian state of Rajasthan, known for its stunning sandstone architecture and its location in the heart of the Thar Desert. The city is home to several impressive palaces and fortresses, as well as unique cultural traditions and handicrafts.",
+		image: "https://cdn.thecodehelp.in/Jaisalmer.jpeg",
+		price: "68,595",
+	},
+	{
+		id: 7,
+		name: "Kochi",
+		info: "Kochi is a port city in the Indian state of Kerala, known for its historic connections to trade and commerce. The city is home to several stunning churches, synagogues, and mosques, as well as a rich culinary tradition that blends Indian, Portuguese, and other influences.",
+		image: "https://cdn.thecodehelp.in/Kochi.jpeg",
+		price: "68,595",
+	},
+	{
+		id: 8,
+		name: "Gwalior",
+		info: "Gwalior district is a district of the Indian state of Madhya Pradesh. This district is under the revenue division of Gwalior. This district is situated between 25 0 34 ‘north and 260’ 21 north latitude and 770 40 ‘east and 780 54’ in the northern part of Gwalior state. This district is spread over a 2002 square meter area, which is 1.1 percent of the total area of ​​Madhya Pradesh state. Gwalior was the capital of central India from 1948 to 1956 but when the central India was connected to Madhya Pradesh, ",
+		image: "https://lh5.googleusercontent.com/p/AF1QipOkxfCaEp_-A_F3GV9Nmn8zII2z1wSXFonavBDc=w675-h390-n-k-no",
+		price: "20,190",
+	},
+	{
+		id: 9,
+		name: "Ambah",
+		info: "It has an average elevation of 161 metres (528 feet). This area is located near the Chambal Ghat. River Chambal flows very near to Ambah and forms the border between M.P. and U.P. and M.P. and Rajasthan. It is famous for its ancient fort and Kakanmath temple.",
+		image: "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/0e/d1/1a/54/photo2jpg.jpg?w=600&h=400&s=1",
+		price: "9,844",
+	},
+];
+
+export default data;
